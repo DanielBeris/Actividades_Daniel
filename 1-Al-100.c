88f8a8ff50bb15314aeb19b654a990694156fd2d
Daniel <reyesberistaindaniel@gmail.com>
2024-05-02 10:47:15 -0600

    02/05/2024

---
+++ 1-Al-100.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+    int i;
+    for (size_t i = 0; i < 101; i++)
+    {
+        printf("%d\n", i);
+    }
+    
+}
---
+++ 1-Al-100V3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+    for (int i = 0; i < 101; i+=2)
+    {
+        printf("%d\n", i);
+    }
+    
+}
